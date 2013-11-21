5b4559a7b8d31637989b49b6e6b1172a91e6859b
Osmar Sanchez <osmar@osmar.(none)>
2013-11-21 08:37:07 -0700

    Inicio Proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TecWeb::Application.config.secret_token = '44ef5e0e1d754f2c02c8d496270c01d76eb0f18ffe5ea9622d487b7850107493bf461ac1933bb278000a773c09cdc6d32e6c7c3dab9e3a0facbdee80799f6a3d'
